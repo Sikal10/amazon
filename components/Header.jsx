36e67fc9cfccb3dae19cb191b3aa2787792aab06
Sikal Sikal <sikal.sikal.ss@gmail.com>
2021-09-05 13:38:59 +0100

    Showing the number of items in the cart

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import logo from "../public/images/amazon_logo.png"
 import {SearchIcon, MenuIcon, ShoppingCartIcon} from "@heroicons/react/outline";
+import {useRouter} from "next/router";
+import {useSelector} from "react-redux";
+import {selectItems} from "../slices/basketSlice";
 
 const Header = () => {
+    const router = useRouter();
+    const items = useSelector(selectItems);
+
     return (
         <header>
             <div className={"bg-amazon_blue p-1 py-2 flex items-center flex-grow"}>
-                <div className={"mt-2 flex flex-grow items-center sm:flex-grow-0"}>
-                    <Image alt={""} src={logo} width={150} height={40} objectFit={"contain"} />
+                <div className={"mt-2 flex flex-grow items-center sm:flex-grow-0 cursor-pointer"}>
+                    <Image onClick={() => router.push("/")} alt={""} src={logo} width={150} height={40} objectFit={"contain"} />
                 </div>
 
                 <div className={"hidden sm:flex items-center h-10 rounded-md flex-grow" +
@@ -27,9 +33,9 @@ const Header = () => {
                         <p className={"font-extrabold md:text-sm"}>& Orders</p>
                     </div>
 
-                    <div className={"link flex items-center relative"}>
+                    <div onClick={() => router.push("/checkout")} className={"link flex items-center relative"}>
                         <span className={"absolute top-0 right-0 md:right-10" +
-                        " h-4 w-4 text-center text-black font-bold rounded-full bg-yellow-400"}>0</span>
+                        " h-4 w-4 text-center text-black font-bold rounded-full bg-yellow-400"}>{items.length}</span>
                         <ShoppingCartIcon className={"h-10"}/>
                         <p className={"font-extrabold hidden md:inline mt-2 md:text-sm"}>Basket</p>
                     </div>
